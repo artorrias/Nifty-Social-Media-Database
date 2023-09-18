62db067ff44ceb4c08d36cfde4a446c454b3e4d0
artorrias <sam.henson1119@outlook.com>
2023-09-18 16:54:41 -0600

    added reactions

--- models/Thought.js
+++ models/Thought.js
@@ -15,6 +15,11 @@ const thoughtSchema = new Schema(
             type: Date,
             default: Date.now(),
         },
+        reactions: [
+            {
+                type: String,
+            }
+        ],
     },
     {
         toJSON: {
--- models/User.js
+++ models/User.js
@@ -21,7 +21,7 @@ const userSchema = new Schema(
         friendCount: {
             type: Number,
             required: true,
-            default: 0,
+            default: thoughts.length,
         },
     },
     {
